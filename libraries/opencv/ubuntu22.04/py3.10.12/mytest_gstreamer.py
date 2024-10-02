41376f9f4aa81908ab731d410d4d385fce6a485d
Federico Lanzani <federico@federicolanzani.com>
2024-10-02 22:41:05 +0200

    :sparkles: Add dependencies.

--- libraries/opencv/ubuntu22.04/py3.10.12/mytest_gstreamer.py
+++ libraries/opencv/ubuntu22.04/py3.10.12/mytest_gstreamer.py
@@ -1,18 +1,16 @@
 import cv2
 
-
 def main():
     unparsed_source = "rtsp://admin:admin@192.168.1.119:554/live/ch1?token=d2abaa37a7c3db1137d385e1d8c15fd2"
     target_shape = (360, 640)
-
+    # uridecodebin uri=rtsp://admin:admin@192.168.1.118:554/live/ch1?token=d2abaa37a7c3db1137d385e1d8c15fd2 ! nvvidconv ! video/x-raw(memory:NVMM) ! nvvidconv ! video/x-raw,format=BGRx ! videorate ! video/x-raw,framerate=7/1 ! videoconvert ! video/x-raw, format=BGR, width=640, height=360 ! appsink drop=1
     source = (
-        f"rtspsrc location={unparsed_source} latency=200 ! queue ! rtph264depay ! h264parse ! "
-        f"nvv4l2decoder ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! "
-        f"video/x-raw,format=BGR, width={target_shape[1]}, height={target_shape[0]} ! "
-        f"queue ! appsink max-buffers=60 drop=true"
+        f"uridecodebin uri={unparsed_source} ! videoconvert ! "
+        f"videoscale ! video/x-raw,width={target_shape[1]},height={target_shape[0]} ! "
+        "appsink"
     )
 
-    cap = cv2.VideoCapture(unparsed_source)
+    cap = cv2.VideoCapture(source, cv2.CAP_GSTREAMER)
 
     if not cap.isOpened():
         print("Error: VideoCapture not opened")
@@ -30,7 +28,6 @@ def main():
 
     cap.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main()
